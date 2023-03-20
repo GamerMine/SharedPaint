1df6851abc6df1aafb0a3711e9f442cf62a119b4
Maxime <maxime-sav@outlook.fr>
2023-03-20 12:57:27 +0100

    Ajout d'outils
     - Affichage des outils (WIP)

--- src/main/java/fr/sae402/sharedpaint/MainController.java
+++ src/main/java/fr/sae402/sharedpaint/MainController.java
@@ -3,14 +3,22 @@ package fr.sae402.sharedpaint;
 import fr.sae402.sharedpaint.metier.OutilForme;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
 public class MainController {
     @FXML
-    private VBox shapeTools;
+    private AnchorPane shapeTools;
 
     @FXML
     public void initialize() {
-
+        for (OutilForme outil : OutilForme.getOutils()) {
+            RadioButton radioButton = new RadioButton("");
+            radioButton.setGraphic(new ImageView(new Image(outil.getIcon().getFile())));
+            shapeTools.getChildren().add(radioButton);
+        }
     }
 }
--- src/main/java/fr/sae402/sharedpaint/metier/OutilForme.java
+++ src/main/java/fr/sae402/sharedpaint/metier/OutilForme.java
@@ -1,20 +1,30 @@
 package fr.sae402.sharedpaint.metier;
 
+import java.net.URL;
 import java.util.ArrayList;
 
 public class OutilForme {
-    // Add any tools that you want here
-    public static final OutilForme RECTANGLE = new OutilForme(Rectangle.class);
-
     private Class<? extends Forme> forme;
+    private URL icon;
     private static ArrayList<OutilForme> outilFormes = new ArrayList<>();
 
-    public OutilForme(Class<? extends Forme> forme) {
+    // Add any tools that you want here
+    public static final OutilForme RECTANGLE = new OutilForme(Rectangle.class, OutilForme.class.getResource("icons/rectangle.png"));
+    public static final OutilForme CERCLE    = new OutilForme(Cercle.class, OutilForme.class.getResource("icons/circle.png"));
+    public static final OutilForme LIGNE     = new OutilForme(Ligne.class, OutilForme.class.getResource("icons/line.png"));
+
+    public OutilForme(Class<? extends Forme> forme, URL icon) {
         this.forme = forme;
+        this.icon = icon;
+        System.out.println(icon);
         OutilForme.outilFormes.add(this);
     }
 
-    public static int getToolsCount() {
-        return OutilForme.outilFormes.size();
+    public URL getIcon() {
+        return this.icon;
+    }
+
+    public static ArrayList<OutilForme> getOutils() {
+        return new ArrayList<>(OutilForme.outilFormes);
     }
 }
