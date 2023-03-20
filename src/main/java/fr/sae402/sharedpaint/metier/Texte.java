b68f9cb0e075ae0dda25fec0bb315f0a997a2ce3
diviini <robin.lebrozec@gmail.com>
2023-03-20 13:18:12 +0100

    Création de la classe Texte

---
+++ src/main/java/fr/sae402/sharedpaint/metier/Texte.java
@@ -0,0 +1,9 @@
+package fr.sae402.sharedpaint.metier;
+
+public class Texte extends Forme {
+    private String texte;
+    public Texte(int posX, int posY, String couleur, String texte ) {
+        super(posX, posY, couleur, true);
+        this.texte = texte;
+    }
+}
