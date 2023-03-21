4b357464af7cec4c9facab9ab04643753a574c42
Alexandre-Crespin <84849257+Alexandre-Crespin@users.noreply.github.com>
2023-03-21 15:19:30 +0100

    Ajout tmp btn

--- src/main/java/fr/sae402/sharedpaint/metier/OutilForme.java
+++ src/main/java/fr/sae402/sharedpaint/metier/OutilForme.java
@@ -15,6 +15,14 @@ public class OutilForme {
     public static final OutilForme CERCLE    = new OutilForme(Cercle.class, SharedPaint.class.getResourceAsStream("icons/circle.png"));
     public static final OutilForme LIGNE     = new OutilForme(Ligne.class, SharedPaint.class.getResourceAsStream("icons/line.png"));
     public static final OutilForme TEXTE     = new OutilForme(Texte.class, SharedPaint.class.getResourceAsStream("icons/text.png"));
+    public static final OutilForme UNDO     = new OutilForme(Texte.class, SharedPaint.class.getResourceAsStream("icons/undo.png"));
+    public static final OutilForme BLACK     = new OutilForme(Texte.class, SharedPaint.class.getResourceAsStream("icons/black.png"));
+    public static final OutilForme WHITE     = new OutilForme(Texte.class, SharedPaint.class.getResourceAsStream("icons/white.png"));
+    public static final OutilForme RED     = new OutilForme(Texte.class, SharedPaint.class.getResourceAsStream("icons/red.png"));
+    public static final OutilForme BLUE     = new OutilForme(Texte.class, SharedPaint.class.getResourceAsStream("icons/blue.png"));
+    public static final OutilForme YELLOW     = new OutilForme(Texte.class, SharedPaint.class.getResourceAsStream("icons/yellow.png"));
+    public static final OutilForme GREEN     = new OutilForme(Texte.class, SharedPaint.class.getResourceAsStream("icons/green.png"));
+
 
     public OutilForme(Class<? extends Forme> forme, InputStream icon) {
         this.forme = forme;
