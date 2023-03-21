72f5970bcfa45a1d1d4d28ecce0008efae9f78a5
diviini <robin.lebrozec@gmail.com>
2023-03-21 16:29:43 +0100

    Maj Metier & MainController
    - maj methode dessiner()

--- src/main/java/fr/sae402/sharedpaint/metier/Metier.java
+++ src/main/java/fr/sae402/sharedpaint/metier/Metier.java
@@ -1,19 +1,32 @@
 package fr.sae402.sharedpaint.metier;
 
 import fr.sae402.sharedpaint.MainController;
+import fr.sae402.sharedpaint.ui.RectangleUI;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 public class Metier {
 
     private MainController controller;
     private Color couleurActuel;
+    private Forme formeActuel;
+    private boolean rempli;
     public Metier(MainController controller )
     {
         this.controller = controller;
     }
 
-    public void dessiner() {
+    public Color getCouleurActuel() {
+        return couleurActuel;
+    }
 
+    public Forme getFormeActuel() {
+        return formeActuel;
+    }
+
+    public boolean isRempli() {
+        return rempli;
     }
 
     public void changerCouleur() {
